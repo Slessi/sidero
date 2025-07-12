7e222c982c06506d584f859153100d0fa3521596
Edward Sammut Alessi <esammutalessi@gmail.com>
2025-07-12 21:35:10 +0200

    Put range calendar behind a popover

--- fe-web-app/src/App.vue
+++ fe-web-app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { RangeCalendar } from '@/components/ui/range-calendar'
 import { getLocalTimeZone } from '@internationalized/date'
 import { useQuery } from '@tanstack/vue-query'
-import { endOfDay, isWithinInterval, parse, startOfDay } from 'date-fns'
+import { endOfDay, isWithinInterval, parse, startOfDay, type Interval } from 'date-fns'
 import type { DateRange } from 'reka-ui'
 import { computed, ref } from 'vue'
 
@@ -47,6 +49,7 @@ const mergedLines = computed(() => {
 })
 
 const search = ref('')
+const popoverOpen = ref(false)
 const dateRange = ref<DateRange>({ start: undefined, end: undefined })
 
 const filteredLines = computed(() => {
@@ -55,26 +58,64 @@ const filteredLines = computed(() => {
 
   if (!trimmedSearch && !start && !end) return mergedLines.value
 
-  const dateInterval = {
-    start: startOfDay((start || end)?.toDate(getLocalTimeZone()) || new Date()),
-    end: endOfDay((end || start)?.toDate(getLocalTimeZone()) || new Date()),
+  let dateInterval: Interval | undefined
+  if (start || end) {
+    dateInterval = {
+      start: startOfDay((start || end)?.toDate(getLocalTimeZone()) || new Date()),
+      end: endOfDay((end || start)?.toDate(getLocalTimeZone()) || new Date()),
+    }
   }
 
   return mergedLines.value.filter(
-    (l) => l.text.includes(trimmedSearch) && isWithinInterval(l.date, dateInterval),
+    (l) =>
+      l.text.includes(trimmedSearch) && (!dateInterval || isWithinInterval(l.date, dateInterval)),
   )
 })
+
+function onDateChange() {
+  popoverOpen.value = false
+}
+
+function clearFilters() {
+  search.value = ''
+  dateRange.value = { start: undefined, end: undefined }
+}
 </script>
 
 <template>
   <main class="px-4 py-2 flex flex-col h-screen items-start gap-2">
     <h1 class="text-2xl">Sidero Logs</h1>
 
-    <RangeCalendar v-model="dateRange" class="rounded-md border" />
-    <Input type="text" placeholder="Search" v-model="search" />
+    <div class="flex gap-2">
+      <Popover v-model:open="popoverOpen">
+        <PopoverTrigger as-child>
+          <Button variant="outline">
+            <template v-if="!dateRange.start && !dateRange.end">Date range</template>
+            <template v-else-if="!dateRange.start!.compare(dateRange.end!)">{{
+              dateRange.start
+            }}</template>
+            <template v-else>{{ dateRange.start }} - {{ dateRange.end }}</template>
+          </Button>
+        </PopoverTrigger>
 
-    <p>{{ dateRange.start?.toString() }}</p>
-    <p>{{ dateRange.end?.toString() }}</p>
+        <PopoverContent class="w-80">
+          <RangeCalendar
+            v-model="dateRange"
+            @update:model-value="onDateChange"
+            class="rounded-md border"
+          />
+        </PopoverContent>
+      </Popover>
+
+      <Input type="text" placeholder="Search" v-model="search" />
+
+      <Button
+        v-if="search || dateRange.start || dateRange.end"
+        variant="ghost"
+        @click="clearFilters"
+        >Clear filters</Button
+      >
+    </div>
 
     <pre class="border-gray-400 border-1 rounded-md px-4 py-2 w-full flex-1 overflow-y-auto">{{
       filteredLines.map((l) => l.text).join('\n')
