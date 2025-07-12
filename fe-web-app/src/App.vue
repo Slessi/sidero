2f919c1bcaf6580cfcaee354cd706e5c7ad369ca
Edward Sammut Alessi <esammutalessi@gmail.com>
2025-07-12 20:41:52 +0200

    Use shadcn components

--- fe-web-app/src/App.vue
+++ fe-web-app/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { RangeCalendar } from '@/components/ui/range-calendar'
+import { getLocalTimeZone } from '@internationalized/date'
 import { useQuery } from '@tanstack/vue-query'
-import { ref, computed } from 'vue'
+import { endOfDay, isWithinInterval, parse, startOfDay } from 'date-fns'
+import type { DateRange } from 'reka-ui'
+import { computed, ref } from 'vue'
 
 const { data } = useQuery({
   queryKey: ['logs'],
@@ -16,23 +21,48 @@ const dateRegex = /^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}/
 
 // This will merge lines of the same log entry into the same line
 const mergedLines = computed(() => {
-  return (data.value?.lines ?? []).reduce<string[]>((prev, curr) => {
-    if (dateRegex.test(curr)) return [...prev, curr]
+  return (data.value?.lines ?? []).reduce<{ date: Date; text: string }[]>((prev, curr) => {
+    const dateMatch = curr.match(dateRegex)
+    if (dateMatch) {
+      return [
+        ...prev,
+        {
+          date: parse(dateMatch[0], 'dd/MM/yyyy HH:mm:ss', new Date()),
+          text: curr,
+        },
+      ]
+    }
 
-    const merged = prev.at(-1)?.concat(`\n${curr}`) ?? curr
+    const lastEntry = prev.at(-1)
+    if (!lastEntry) return prev
 
-    return [...prev.slice(0, prev.length - 1), merged]
+    return [
+      ...prev.slice(0, prev.length - 1),
+      {
+        ...lastEntry,
+        text: `${lastEntry.text}\n${curr}`,
+      },
+    ]
   }, [])
 })
 
 const search = ref('')
+const dateRange = ref<DateRange>({ start: undefined, end: undefined })
 
 const filteredLines = computed(() => {
   const trimmedSearch = search.value.trim()
+  const { start, end } = dateRange.value
 
-  if (!trimmedSearch) return mergedLines.value
+  if (!trimmedSearch && !start && !end) return mergedLines.value
 
-  return mergedLines.value.filter((line) => line.includes(trimmedSearch))
+  const dateInterval = {
+    start: startOfDay((start || end)?.toDate(getLocalTimeZone()) || new Date()),
+    end: endOfDay((end || start)?.toDate(getLocalTimeZone()) || new Date()),
+  }
+
+  return mergedLines.value.filter(
+    (l) => l.text.includes(trimmedSearch) && isWithinInterval(l.date, dateInterval),
+  )
 })
 </script>
 
@@ -40,11 +70,14 @@ const filteredLines = computed(() => {
   <main class="px-4 py-2 flex flex-col h-screen items-start gap-2">
     <h1 class="text-2xl">Sidero Logs</h1>
 
-    <input type="text" class="border-red-500 border-2 rounded-md" v-model="search" />
-    <p>{{ search }}</p>
+    <RangeCalendar v-model="dateRange" class="rounded-md border" />
+    <Input type="text" placeholder="Search" v-model="search" />
+
+    <p>{{ dateRange.start?.toString() }}</p>
+    <p>{{ dateRange.end?.toString() }}</p>
 
     <pre class="border-gray-400 border-1 rounded-md px-4 py-2 w-full flex-1 overflow-y-auto">{{
-      filteredLines.join('\n')
+      filteredLines.map((l) => l.text).join('\n')
     }}</pre>
   </main>
 </template>
